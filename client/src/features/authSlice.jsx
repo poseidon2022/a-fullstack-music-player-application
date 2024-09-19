affbd40541ed04afeb9f912ca279d9448bb238e2
poseidon2022 <kidusm82@gmail.com>
2024-09-19 12:17:03 +0300

    authslice extra reducers

--- client/src/features/authSlice.jsx
+++ client/src/features/authSlice.jsx
@@ -1,4 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit"
+import {userLogin} from "../actions/authAction"
 
 const initialState = {
     loading: false,
@@ -13,7 +14,24 @@ const authSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers : (builder) =>  {
+        builder
+            .addCase(userLogin.pending, (state) => {
+                state.loading = true
+                state.error = null
+            })
         
+        builder
+            .addCase(userLogin.fulfilled, (state, {payload}) => {
+                state.loading = false,
+                state.userInfo = payload.userInformation,
+                state.userToken = payload.userInformation.accessToken
+            })
+        
+        builder
+            .addCase(userLogin.rejected, (state, {payload}) => {
+                state.loading = false
+                state.error = payload
+            })
     }
 })
 
