282cc577383c0231b6b4a580adce6e86bf539693
poseidon2022 <kidusm82@gmail.com>
2024-09-19 18:38:33 +0300

    song model

---
+++ server/models/song.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const songSchema = new mongoose.Schema({
+    song_name : {
+        type : String,
+        required: true
+    }, 
+    artist_name : {
+        type : String,
+        required: true
+    }, 
+    audio_url : {
+        type : String,
+        required : true
+    },
+    image_url : {
+        type : String,
+        required: true
+    },
+    user_id : {
+        type : String,
+        required: true
+    }
+})
+
+const Song = mongoose.model("songs", songSchema)
+module.exports = Song
